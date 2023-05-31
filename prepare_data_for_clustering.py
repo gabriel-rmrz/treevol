ab4db7b0a837b052c60ffc1428bda9d1054e63da
gabriel-rmrz <gabriel-rmrz@users.noreply.github.com>
2023-05-31 09:52:01 +0200

    prepare sample script working

--- prepare_data_for_clustering.py
+++ prepare_data_for_clustering.py
@@ -4,75 +4,125 @@ import laspy
 import numpy as np
 import pandas as pd
 
-# Lettura della nuvola di punti
-input_file = '../pointnet2/data/Area_2_LAS_5.las'
-h5_outfile = h5py.File('z_slice.h5', 'w')
-las = laspy.read(input_file)
-'''
-print(list(las.point_format.dimension_names))
-exit()  
-'''
 
-point_data = np.stack([las.X, las.Y, las.Z], axis=0).transpose((1,0))
-point_color = np.stack([las.red, las.green, las.blue], axis=0).transpose((1,0))
-point_color  = point_color/point_color.max()
+def make_slice(outliers, slice_limits):
+  for i in range(len(slice_limits) -1):
+    outfile_name = 'data/slice_z_%d_%d.h5' % (slice_limits[i], slice_limits[i+1])
+    h5_outfile = h5py.File(outfile_name, 'w')
+    '''
+    min
+    -1743.438
+    -2445.0
+    -157.327
+    max
+    9023.125
+    4425.0
+    481.514
+    '''
+    tree_bounding_box = o3d.geometry.AxisAlignedBoundingBox(
+        min_bound=(913310, 24158, slice_limits[i]),
+        max_bound=(952870, 63934, slice_limits[i+1])
+    #    min_bound=(-1743438, -2445000, slice_limits[i]),
+    #    max_bound=(9023125, 4425000, slice_limits[i+1])
+    #    min_bound=(-1743438, -2445000, -157327),
+    #    max_bound=(9023125, 4425000, 481514)
+    #    min_bound=(913310, 24158, -4025),
+    #    max_bound=(952870, 63934, 10285)
+    #    min_bound=(-00, 101955, slice_limits[i]),
+    #    max_bound=(-27774, 3839063, slice_limits[i+1])
+    )
+    
+    # Filtraggio della nuvola di punti sull'albero
+    tree_geom = outliers.crop(tree_bounding_box)
+    #tree_geom = outliers
 
-geom = o3d.geometry.PointCloud()
-geom.points = o3d.utility.Vector3dVector(point_data)
-geom.colors = o3d.utility.Vector3dVector(point_color)
+    '''
+    # Visualization of the results.
+    viewer = o3d.visualization.Visualizer()
+    viewer.create_window()
+    viewer.add_geometry(tree_geom)
+    #viewer.add_geometry(tree_mesh)
+    opt = viewer.get_render_option()
+    opt.show_coordinate_frame = True
+    opt.background_color = np.asarray([0.5, 0.5, 0.5])
+    viewer.run()
+    viewer.destroy_window()
+    '''
+    
+    xyz_arr = np.asarray(tree_geom.points)/1000 # Changing unit from mm to m
+    xyz_colors_arr = np.asarray(tree_geom.colors)/256 #Normalising colors 
+    print(xyz_arr[:,0])
 
-# Definizione del filtro di rimozione del pavimento
-#plane_model, inliers = geom.segment_plane(distance_threshold=.1,  ransac_n=3, num_iterations=500)
-plane_model, inliers = geom.segment_plane(distance_threshold=500.,  ransac_n=3, num_iterations=500)
-outliers = geom.select_by_index(inliers, invert=True)
+    print("min")
+    print(xyz_arr[:,0].min())
+    print(xyz_arr[:,1].min())
+    print(xyz_arr[:,2].min())
+    print("max")
+    print(xyz_arr[:,0].max())
+    print(xyz_arr[:,1].max())
+    print(xyz_arr[:,2].max())
+    xyz_df = pd.DataFrame(xyz_arr, columns=['x0', 'x1', 'x2'])
+    xyz_df['x0'] = xyz_df['x0'] - xyz_df['x0'].min()
+    xyz_df['x1'] = xyz_df['x1'] - xyz_df['x1'].min()
+    xyz_df['x2'] = xyz_df['x2'] - xyz_df['x2'].min()
+    xyz_df['weights']=1
+    xyz_df['r'] = xyz_colors_arr[:,0] 
+    xyz_df['g'] = xyz_colors_arr[:,1] 
+    xyz_df['b'] = xyz_colors_arr[:,2] 
+    
+    #xyz_df.to_hdf('z_slice.h5','df', mode='w')
+    h5_outfile.create_dataset('slice', data=xyz_df)
+    h5_outfile.close()
+  return 0
+  
 
-#o3d.visualization.draw_geometries([outliers])
+def main():
+  # Lettura della nuvola di punti
+  input_file = '../pointnet2/data/Area_2_LAS_5.las'
+  #input_file = 'data/Roma_Milvio_ridotta.las'
+  #input_file = 'data/Roma_Milvio.las'
+  #input_file = '../pointnet2/data/Area1_01_Output_laz1_4_colourised.laz'
+  las = laspy.read(input_file)
+  '''
+  print(list(las.point_format.dimension_names))
+  exit()  
+  '''
+  
+  point_data = np.stack([las.X, las.Y, las.Z], axis=0).transpose((1,0))
+  point_color = np.stack([las.red, las.green, las.blue], axis=0).transpose((1,0))
+  point_color  = point_color/point_color.max()
+  
+  geom = o3d.geometry.PointCloud()
+  geom.points = o3d.utility.Vector3dVector(point_data)
+  geom.colors = o3d.utility.Vector3dVector(point_color)
+  
 
-# Definizione della bounding box dell'albero
-''' 
-Initial limits
-X
-952870
-913310
-Y
-63934
-24158
-Z
-10285
--4026
-'''
-tree_bounding_box = o3d.geometry.AxisAlignedBoundingBox(
-    min_bound=(913310, 24158, 1000),
-    max_bound=(952870, 63934, 1100)
-#    min_bound=(913310, 24158, -4025),
-#    max_bound=(952870, 63934, 10285)
-)
+  # Definizione del filtro di rimozione del pavimento
+  #plane_model, inliers = geom.segment_plane(distance_threshold=.1,  ransac_n=3, num_iterations=500)
+  plane_model, inliers = geom.segment_plane(distance_threshold=500.,  ransac_n=3, num_iterations=500)
+  outliers = geom.select_by_index(inliers, invert=True)
+  
+  #o3d.visualization.draw_geometries([outliers])
+  
+  # Definizione della bounding box dell'albero
+  ''' 
+  Initial limits
+  X
+  952870
+  913310
+  Y
+  63934
+  24158
+  Z
+  10285
+  -4026
+  '''
+  #slice_limits = [1300, 1400, 1500, 1600, 1700, 1800, 1900, 2000, 2100]
+  #slice_limits = [2000, 2200, 2400, 2600, 2800, 3000]
+  slice_limits = [2000, 2300, 2600, 2900, 3200, 3500]
+  #slice_limits = [30000, 40000]
+  make_slice(outliers, slice_limits)
 
-# Filtraggio della nuvola di punti sull'albero
-tree_geom = outliers.crop(tree_bounding_box)
-
-# Visualization of the results.
-viewer = o3d.visualization.Visualizer()
-viewer.create_window()
-viewer.add_geometry(tree_geom)
-#viewer.add_geometry(tree_mesh)
-opt = viewer.get_render_option()
-opt.show_coordinate_frame = True
-opt.background_color = np.asarray([0.5, 0.5, 0.5])
-viewer.run()
-viewer.destroy_window()
-# Plotting xy projection
-
-xyz_arr = np.asarray(tree_geom.points)/1000 # Changing unit from mm to m
-xyz_df = pd.DataFrame(xyz_arr, columns=['x0', 'x1', 'x2'])
-xyz_df['x0'] = xyz_df['x0'] - xyz_df['x0'].min()
-xyz_df['x1'] = xyz_df['x1'] - xyz_df['x1'].min()
-xyz_df['x2'] = xyz_df['x2'] - xyz_df['x2'].min()
-xyz_df['weights']=1
-
-#xyz_df.to_hdf('z_slice.h5','df', mode='w')
-h5_outfile.create_dataset('slice', data=xyz_df)
-h5_outfile.close()
-
-
+if __name__=='__main__':
+  main()
 
